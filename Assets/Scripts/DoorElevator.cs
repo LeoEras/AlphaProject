763f94532ee100eda611b307a3b9895e85b509d9
Xidrovo <xidrovo@hotmail.com>
2015-05-09 21:53:30 -0400

    Elevador con puertas! (Errores corregidos)
    >El elevador te manda lentamente al centro del mismo.
    >La puerta se cierra y abre cuando ésto ocurre
    >No se puede mover mientras esto ocurre
    >Nota: Modificar script de salto... sólo debería saltar cuando está en
    estado "main" D:
    > asdas
    >dsadsa~

---
+++ Assets/Scripts/DoorElevator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorElevator : MonoBehaviour {
+
+	public Transform RightDoor, LeftDoor;
+	public float Speed = 5f, TempSpeed, CloseSpeed, Limit;
+	private Vector3 RightVectIn, LeftVectIn, RightLimit, LeftLimit;
+	private SpriteRenderer Right, Left;
+	public ElevatorBehaviour Lift;
+
+
+	void Start()
+	{
+		RightVectIn = RightDoor.position;
+		LeftVectIn = LeftDoor.position;
+		TempSpeed = Speed;
+		CloseSpeed = Speed;
+
+		Right = RightDoor.GetComponent<SpriteRenderer> ();
+		Left = LeftDoor.GetComponent<SpriteRenderer> ();
+	}
+
+	void Update()
+	{
+		RightVectIn.y = RightDoor.position.y;
+		RightLimit.x = RightVectIn.x + Limit;
+		LeftVectIn.y = LeftDoor.position.y;
+
+	}
+
+	public void OpenDoor()
+	{
+		RightDoor.transform.Translate (Vector3.right * TempSpeed/10 * Time.deltaTime);
+		LeftDoor.transform.Translate (Vector3.left * TempSpeed/10 * Time.deltaTime);
+
+		if ((float)RightDoor.position.x >= (float)RightLimit.x) 
+		{
+			TempSpeed = 0;
+			Lift.SetOpening(false);
+
+			Right.sortingOrder = 1;
+			Left.sortingOrder = 1;
+		}
+				else 
+						TempSpeed = Speed;
+	}
+
+	public void CloseDoor()
+	{
+		RightDoor.transform.Translate (Vector3.left * CloseSpeed/10 * Time.deltaTime);
+		LeftDoor.transform.Translate (Vector3.right * CloseSpeed/10 * Time.deltaTime);
+
+		Right.sortingOrder = 3;
+		Left.sortingOrder = 3;
+		if (RightDoor.position.x <= RightVectIn.x) 
+		{
+			Lift.SetEnding (true);
+			CloseSpeed = 0;
+		} else
+			CloseSpeed = Speed;
+	}
+}
--- Assets/Scripts/ElevatorBehaviour.cs
+++ Assets/Scripts/ElevatorBehaviour.cs
@@ -11,52 +11,89 @@ public class ElevatorBehaviour : MonoBehaviour {
 	// > the Height the Lift will up
 	// >A cooldown that only works to the player to not to move for a sec when the lift reach the top.
 	public float Speed, Height, CoolDown = 5f;
-	//there's no reason to make this vector 3 public.
-	private Vector3 Vect, Posi, Prueba;
 	//Rigibody to use some methods for the lift and for the player.
 	public Rigidbody2D Lift, PlayerBody;
 	//This forces "ignores" other forces. At least gravity.
 	public ConstantForce2D ConstantLift;
+	public DoorElevator Door;
+
+	//there's no reason to make this vector 3 public.
+	private Vector3 Vect, Posi, Prueba;
+	//this booleans will help me to open or close the door.
+	private bool Opening = false, Closed = true;
+	private float PlayerGravity;
 
 	void OnCollisionStay2D(Collision2D Coll)
 	{
 		//technically this 'if' means if the user is almost at the middle of the Lift, the lift can move and is touching the lift.
 		if (Turner.GetStatus() && Coll.collider.name.Equals ("Player")) {
-
+			//This little block helps to move the user to the center of the lift.
 			if(CoolDown == 5)
 			{
-
+				//This just going to make the movement automacly to the center... and rotate the player if is necessary.
 				if(Player.position.x - transform.position.x > 0)
 					Player.eulerAngles = new Vector2 (0, 180);
 				else
 					Player.eulerAngles = new Vector2 (0, 0);
-
+				//This one moves the player... the another just makes it rotate.
 				Player.position = Vector3.MoveTowards(Player.position, Posi, Attributes.speed/150);
 			}
 			if ( (float) Player.position.x == Vect.x)
 			{
-				CoolDown = 2;
+				//Making the gravity of the player to "0".
+				PlayerBody.gravityScale = 0;
+				//HERE'S AND ERROR!!... well is not aaan error... is just that... something...
+				//It's a lie it's a lie, there's no error~
+				Lift.isKinematic = false;
+
 				//If that happends, the player won't move until reach the max. Height. that's the "stun" state of the player.
 				Attributes.state = (int)Attributes.States.Stun;
-				//As I said, the Lift will have a contant force, so will ignore gravity when is going up.
-				ConstantLift.force = Vector2.up * Speed;
-				Lift.isKinematic = false;
+
+				if (CoolDown > 2)
+				{
+					//So now, the door will open!
+					Opening = true;
+					Closed = false;
+				}
+				CoolDown = 2;
+				//read the "DoorElevator.cs" Script to understand that once is open... the boolean "opening" becomes false
+				if (Opening == false)
+				{
+					//Once is fully open... this have to close~
+					Door.CloseDoor();	
+				}
+				//If is closed and won't open... well... the lift will rise (the "isKinematic" option must been here...
+				//But for some reason when I put it here... well... just stop working =(
+				if(Opening == false && Closed)
+				{
+					//As I said, the Lift will have a contant force, so will ignore gravity when is going up.
+					ConstantLift.force = Vector2.up * Speed;
+					//This will give the normal gravity to the player
+					PlayerBody.gravityScale = PlayerGravity;
+					//Going to stop ignore it!
+					Physics2D.IgnoreLayerCollision (8, 8,false);
+				}
 			}
 		}
 		//So finally reach the top!... this what happend...
 		if ((int)transform.position.y >= (int)Vect.y)
 		{
-			//The player may move again... just if he want to :D
-			Attributes.state = (int)Attributes.States.Main;
-			if(CoolDown >= 0)
+			if(CoolDown >= 0 )
 			{
+				//This will open the door one more time!
+				Opening = true;
 				//This will make the player not to jump when the Lift suddenly stop.
 				//It actually make not to move anywhere... but just for a few fraction of seconds.
 				PlayerBody.velocity = Vector3.zero;
 				//Once the lift reach the top... there's a tiny cooldown, that actually ends few fraction of seconds the player reach the top.
 				CoolDown -= 1;
 			}
-
+			//Now the player won't move until the door is fully open!... because that's elegant(?)
+			if (Opening == false)
+			{
+				//The player may move again... just if he want to :D
+				Attributes.state = (int)Attributes.States.Main;
+			}
 
 		}
 	}
@@ -66,6 +103,9 @@ public class ElevatorBehaviour : MonoBehaviour {
 		//Have to change this to make the vector more... dinamyc.
 		Posi = transform.position;
 		Lift.isKinematic = true;
+		PlayerGravity = PlayerBody.gravityScale;
+		//To ignore the Ground Layer.
+		Physics2D.IgnoreLayerCollision (8, 8);
 	}
 
 
@@ -77,6 +117,10 @@ public class ElevatorBehaviour : MonoBehaviour {
 		Vect = transform.position;
 		Vect.y = Height;
 		Debug.DrawLine (transform.position, Vect, Color.blue);
+		//Well if is opening... the door must open~ 
+		//Read the "DoorElevator.cs" script to understan that once is fully open.. the "Opening" parameter becomes false
+		if (Opening)
+						Door.OpenDoor ();
 
 	}
 
@@ -88,5 +132,16 @@ public class ElevatorBehaviour : MonoBehaviour {
 			//Making it kinematic just to be sure that the lift won't move... anymore!
 			Lift.isKinematic = true;
 		}
+	}
+
+	//Just setters used on the "DoorElevator.cs"
+	public void SetOpening(bool IsOpening)
+	{
+		Opening = IsOpening;
+	}
+
+	public void SetEnding(bool IsClosed)
+	{
+		Closed = IsClosed;
 	}
 }
